f17bf8629148fa2f327ac63456fa68c3fe5604ae
Vithrandir <2541556282@qq.comm>
2017-12-14 15:18:08 +0800

    play_again0

--- play_again0.c
+++ play_again0.c
@@ -8,6 +8,8 @@ int get_reponse(char *);
 int main(int argc, char const *argv[])
 {
 	int response;
+	tty_mode(0);
+	set_crmode();
 	response = get_response(QUESTION);
 	return response;
 }
@@ -21,6 +23,26 @@ int get_response(char * question){
 			case 'n':
 			case 'N':
 			case EOF:return 1;
+			default:
+				printf("\ncannot understand %c,",input);
+				printf("Please type y or no\n");
 		}
+	}
+}
+set_crmode(){
+	struct termios ttystate;
+	tcgetattr(0,&ttystate);
+	ttystate.c_lflag &= ~ICANON;
+	ttystate.c_cc[VMIN] = 1;
+	tcsetattr(0,TCSANOW,&ttystate);
+}
+
+tty_mode(int how){
+	static struct termios original_mode;
+	if(how == 0){
+		tcgetattr(0,&original_mode);
+	}
+	else{
+		return tcsetattr(0,TCSANOW,&original_mode);
 	}
 }
